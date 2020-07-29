7ad53203d5420020afc74f1c6dacdf77b061ec0e
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-29 15:47:49 -0700

    remove build script

--- build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    rdxl_static::build();
-}
--- src/site.rs
+++ src/site.rs
@@ -1,4 +1,2 @@
 pub fn run() {
-    rdxl_static::dot_to_file("/index.html",&crate::index()).unwrap();
-    rdxl_static::dot_to_file("/page1.html",&crate::page1()).unwrap();
 }
